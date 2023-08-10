cf5ad41e68319f45af91491f1495965aa730f804
Reza Fathi <rezaf@wabisabi.ltd>
2023-08-10 23:32:32 +0330

    feat(): setup typeorm

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,8 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class UserEntity {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   username: string;
+
+  @Column()
   password: string;
+
+  @Column()
   email: string;
+
+  @Column()
   phone: string;
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  providers: [UserService],
+  controllers: [UserController],
+})
 export class UserModule {}
